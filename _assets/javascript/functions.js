344da5921f87c57942fd62c87e3245a191a0bb99
mel pierre <mel.pierre@digital.justice.gov.uk>
2014-12-01 12:48:05 +0000

    added new js file to write functions for the pages

---
+++ _assets/javascript/functions.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+//
+$('.section').mouseover(function() {
+  /* Act on the event */
+  console.log('working');
+
+// });(function() {
+//   console.log('working');
+//   /* Act on the event */
+//   // var $this = $(this);
+//   // $siblings = $this.parent().children('section').hide();
+
+
+});
+
+});
