6ee5e5de1687f28f6d3c9dfccbc4f2314aef88d8
mel pierre <mel.pierre@digital.justice.gov.uk>
2014-12-04 16:03:45 +0000

    refactored code for page function

--- _assets/javascript/functions.js
+++ _assets/javascript/functions.js
@@ -1,57 +1,46 @@
-$(document).ready(function() {
-  'use strict';
-
-
-  // $(window).load(function() {
- 
-
-  //cache all the responses on the page
-  var $allResponses = $('h2').append('<img src="/images/expand.png">').nextUntil('h2');
-
-  // hide all the responses onload
-  $allResponses.hide();
-
-  //add an click event handle on body to listen for clicks on h2
-  $('body').on('click', 'h2', function() {
-    //cache the h2 that triggered the event
-    var $element = $(this),
-      //Cache the content between the clicked h2 and the next H2    
-      $response = $element.nextUntil('h2');
-    // $icon = $('h2').append( "<span>+</span>" );
-
-    //looking at all the responses 
-    $allResponses
-    //Find all the visible responses
-      .filter(':visible')
-      //That are not the current set of responses
-      .not($response)
-      //hide them
-      .hide();
-
-    //if the current set of response is visible then hide it else show it
-    if ($response.filter(':visible').length != 0) {
-      $response.hide();
-      // $icon = $('h2').append( "<span>+</span>" );
-    } else {
-      $response.show();
-      // $icon = $('h2').append( "<span>-</span>" );
-      // $('h2').append('<img src="/images/expand.png">')
-    };
-
-
-    // });
-
-
-  });
 
   moj.Modules.functions = {
     init: function() {
       this.bindEvents();
     },
     bindEvents: function() {
+      //cache all the responses on the page
+      var $allResponses = $('h2').nextUntil('h2');
 
+      // hide all the responses onload
+      $allResponses.hide();
+
+      //add an click event handle on body to listen for clicks on h2
+      $('body').on('click', 'h2', function() {
+        //cache the h2 that triggered the event
+        var $element = $(this),
+          //Cache the content between the clicked h2 and the next H2    
+          $response = $element.nextUntil('h2'),
+          $icon = $element.find('span');
+
+        //looking at all the responses 
+        $allResponses
+          .filter(':visible') //Find all the visible responses
+          .not($response) //That are not the current set of responses
+          .hide(); //hide them
+
+        //if the current set of response is visible then hide it else show it
+        if ($response.filter(':visible').length != 0) {
+          $response.hide();
+          $element.removeClass('is-open');
+
+        } else {
+          $response.show();
+          $element.addClass('is-open');
+
+
+        };
+
+
+
+      });
 
 
     }
   };
-}());
+// }());
