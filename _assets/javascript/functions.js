ab6f0a6c4df8557adb99fe495929a430f2266bc2
mel pierre <mel.pierre@digital.justice.gov.uk>
2014-12-11 16:57:14 +0000

    added functionality to copy and past URL in a new window and open specific section

--- _assets/javascript/functions.js
+++ _assets/javascript/functions.js
@@ -3,57 +3,98 @@
 
   moj.Modules.functions = {
     init: function() {
+      this.cacheEls();
       this.bindEvents();
+      this.render();
+      // this.FindElement();
+    },
+
+    cacheEls: function () {
+      this.$allResponses = $('h2').nextUntil('h2'); //cachEls the elemtns we are going to use the most , do it once 
     },
 
     bindEvents: function() {
-        //cache all the responses on the page
-        var $allResponses = $('h2').nextUntil('h2');
+      var that = this;
 
-        // hide all the responses onload
-        $allResponses.hide();
+      $('body').on('click', 'h2', function(e) {
+        that.toggleSection($(this));
+      });
+    }, //end bind events
 
-        //add an click event handle on body to listen for clicks on h2
-        $('body').on('click', 'h2', function() {
-          //cache the h2 that triggered the event
-          var $element = $(this),
-            //Cache the content between the clicked h2 and the next H2    
-            $response = $element.nextUntil('h2'),
-            $icon = $element.find('span');
+    toggleSection: function($element) {
+      //Cache the content between the clicked h2 and the next H2    
+      var $response = $element.nextUntil('h2');
+      var $icon = $element.find('span');
 
-          //looking at all the responses 
-          $allResponses
-            .filter(':visible') //Find all the visible responses
-            .not($response) //That are not the current set of responses
-            .hide(); //hide them
+      //looking at all the responses 
+      this.$allResponses
+        .filter(':visible') //Find all the visible responses
+        .not($response) //That are not the current set of responses
+        .hide(); //hide them
 
-          //if the current set of response is visible then hide it else show it
-          if ($response.filter(':visible').length != 0) {
-            $response.hide();
-            $element.removeClass('is-open');
+      //if the current set of response is visible then hide it else show it
+      if ($response.filter(':visible').length != 0) {
+        $response.hide();
+        $element.removeClass('is-open');
+      } else {
+        $response.show();
+        $element.addClass('is-open');
+      };
+    },
 
-          } else {
-            $response.show();
-            $element.addClass('is-open');
-          };
+    FindElement: function() {
+        var that = this;
+        // that.toggleSection();
 
-        });
+        // var $TheID = $(document).showPage(location.hash); // does include the # for the record
 
-        $('h3').append(function(index, el) {
-          //for each h3 found append "" link
-          // var %name = $(this).text()
-          return '<a href="#' + $(this).attr('id') + '" class="HashLink"> #</a>';
-        })
+        var $URLstring = document.location;
+        console.log($URLstring);
 
-        $('h4').append(function(index, el) {
-          //for each h3 found append "" link
-          return '<a href="#' + $(this).attr('id') + '" class="HashLink"> #</a>';
-        })
+        var hash = window.location.hash; //get the hash for the url string
+        // $(hash).show(); //displays the hash link on the page
+        console.log('The hash: ' + hash);
+
+        var hash2 = document.URL.substr(document.URL.indexOf('#') + 1)
+        console.log('The hash index: ' + hash2);
+
+        var id = hash2;
+        console.log('the id : ' + id);
+        var element = document.getElementById(id); //find the element by its ID
+        console.log('The element: ' + element);
+
+        // if the hash is hidden 
+        if ($(element).is(':hidden') === true) {
+
+          console.log('object (h3/h4) id shown as Hidden: ' + id);
+
+          //find the top h2 and unhide the section 
+          var $response = $('h2').prev().nextUntil('h2');
+          $response.show();
+          $response.addClass('is-open');
+
+        };
 
 
-      } //end bind events
+    },
 
+    render: function () { // what we see on the page 
+      this.$allResponses.hide(); // new call to render the h3 / h4 href below.........
 
+      $('h3').append(function(index, el) {
+        //for each h3 found append "" link
+        return '<a href="#' + $(this).attr('id') + '" class="HashLink"> #</a>';
+      })
+      $('h4').append(function(index, el) {
+        //for each h4 found append "" link
+        return '<a href="#' + $(this).attr('id') + '" class="HashLink" > #</a>';
+      })
+
+      var hash = window.location.hash;
+      if (hash) {
+        $(hash).prevAll('h2').first().click();
+      }
+    }
 
   }; //moj.Modules.functions
 }());
